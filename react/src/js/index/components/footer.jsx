b15c0b67f21064103e537e794691980c46ce3cca
ncyu <naichunyu@aliyun.com>
2017-04-04 00:50:16 +0800

    修改页面底部组件

--- react/src/js/index/components/footer.jsx
+++ react/src/js/index/components/footer.jsx
@@ -1,38 +1,61 @@
 /**
- * [footer 编辑组件]
+ * [TodoFooter 底部路由切换组件【属于view层】]
  */
-var app=app||{};
-(function(){
+var app = app || {};
+
+(function () {
 	'use strict';
 
-	app.TodoFooter=React.createClass({
-		render:function(){
-			var activeTodoWord=app.Utils.pluralize({this.props.count,'item'});
-			var clearButton=null;
-			if(this.props.completedCount>0){
-				clearButton=(
-					<button className="clear-completed" oncLick={this.props.onclearCompleted}> Clear completed </button>
+	app.TodoFooter = React.createClass({
+		render: function () {
+			var activeTodoWord = app.Utils.pluralize(this.props.count, 'item');
+			var clearButton = null;
+
+			if (this.props.completedCount > 0) {
+				clearButton = (
+					<button
+						className="clear-completed"
+						onClick={this.props.onClearCompleted}>
+						Clear completed
+					</button>
 				);
 			}
-			//有return，且是复合组件：用括号()
+
+			var nowShowing = this.props.nowShowing;
 			return (
 				<footer className="footer">
-				    <span className="todo-count">
-				        <strong>{this.props.count}</strong>{activeTodoWord} left
-				    </span>
-				    <ul className="filters">
-				        <li>
-					        <a href="#/" className={classNames({selected:nowshowing===app.ALL_TTODOS})}>ALL</a>
-				        </li>
-				        <li>
-					        <a href="#/active" className={classNames({selected:nowshowing===app.ACTIVE_TTODOS})}>Active</a>
-				        </li>
-				        <li>
-					        <a href="#/completed" className={classNames({selected:nowshowing===app.COMPLETED_TTODOS})}>Completed</a>
-				        </li>
-				    </ul>
+					<span className="todo-count">
+						<strong>{this.props.count}</strong> {activeTodoWord} left
+					</span>
+					<ul className="filters">
+						<li>
+							<a
+								href="#/"
+								className={nowShowing===app.ALL_TTODOS?"selected":" "}>
+									All
+							</a>
+						</li>
+						{' '}
+						<li>
+							<a
+								href="#/active"
+								className={nowShowing===app.ACTIVE_TTODOS?"selected":" "}>
+									Active
+							</a>
+						</li>
+						{' '}
+						<li>
+							<a
+								href="#/completed"
+								className={nowShowing===app.COMPLETED_TTODOS?"selected":" "}>
+									Completed
+							</a>
+						</li>
+					</ul>
+					{clearButton}
 				</footer>
-			)
+			);
 		}
 	});
 })();
+
