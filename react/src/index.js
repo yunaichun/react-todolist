ee1454b649739c45296eb061e4dfb6d476b4d037
wufang <naichunyu@aliyun.com>
2017-11-14 19:35:02 +0800

    添加中间件:store->next->action[执行next(action)相当于store.dispatch，同时返回store形成链式调用]

--- react/src/index.js
+++ react/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 /**此处不引入react-router【必须先引入React组件】**/
 import { HashRouter, Route } from 'react-router-dom';
-
 //容器组件
 import App from './containers/App';
 //顶层reducers
 import todoApp from './reducers/index';
+import { logger, crashReporter } from './MiddlewareActions';
+
 //传入reducers创建Redux Store
-const store = createStore(todoApp);
+const store = createStore(
+  todoApp,
+  // applyMiddleware() tells createStore() how to handle middleware
+  applyMiddleware(logger, crashReporter)
+);
 
 
 ReactDOM.render(
