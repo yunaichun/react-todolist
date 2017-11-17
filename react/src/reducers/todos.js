0b0536f78f8b82244af3646b19a53171f00bf08c
wufang <naichunyu@aliyun.com>
2017-11-17 22:39:32 +0800

    添加reducer enhancers，返回有历史记录的reducers

--- react/src/reducers/todos.js
+++ react/src/reducers/todos.js
@@ -3,7 +3,7 @@
  * 第二个参数是dispatch传入的参数
  * 所以dispatch相当于执行reducer函数
  */
-export default (state = [], action) => {
+function todos(state = [], action) {
   switch (action.type) {
     case 'ADD_TODO': //添加条目
       return [
@@ -68,4 +68,51 @@ export default (state = [], action) => {
     default://默认返回初始state
       return state;
   }
-};
+}
+
+function undoable(reducer) {
+  // 以一个空的 action 调用 reducer 来产生初始的 state
+  const initialState = {
+    past: [],
+    present: reducer(undefined, {}),
+    future: []
+  };
+
+  // 返回一个可以执行撤销和重做的新的reducer
+  return function (state = initialState, action) {
+    const { past, present, future } = state;
+
+    switch (action.type) {
+      case 'UNDO':
+        const previous = past[past.length - 1];
+        const newPast = past.slice(0, past.length - 1);
+        return {
+          past: newPast,
+          present: previous,
+          future: [present, ...future]
+        };
+      case 'REDO':
+        const next = future[0];
+        const newFuture = future.slice(1);
+        return {
+          past: [...past, present],
+          present: next,
+          future: newFuture
+        };
+      default:
+        // 将其他 action 委托给原始的 reducer 处理
+        const newPresent = reducer(present, action);
+        if (present === newPresent) { //不做此判断初始past为两个空数组
+          return state;
+        }
+        return {
+          past: [...past, present],
+          present: newPresent,
+          future: []
+        };
+    }
+  };
+}
+//Reducer Enhancers类似combineReducers()，传入reducers，返回新的reducers
+//传的参数是一个函数，返回一个函数
+export default undoable(todos);
